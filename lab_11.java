db7bc3175dee5ea4310f280ea085f6269a749bb2
V-Mani <95094593+V-Mani@users.noreply.github.com>
2022-05-19 15:44:17 +0530

    a

--- lab_11.java
+++ lab_11.java
@@ -1,3 +1,19 @@
+import java.io.*;
+import java.util.ArrayList;
+
+
 public class lab_11 {
-    
+    public static void main(String[] args) {
+
+		ArrayList<String> arrlist = new ArrayList<String>();
+
+		arrlist.add("aaa");
+		arrlist.add("bbb");
+		arrlist.add("ccc");
+		arrlist.add("ddd");
+
+		for (String number : arrlist) {
+			System.out.println("Number = " + number);
+		}
+	} 
 }
